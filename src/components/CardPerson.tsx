e779a3cc7acf7d13c5cd5f9b7c0216fb9769b6eb
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2021-02-27 20:45:57 -0300

    cards awards

---
+++ src/components/CardPerson.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/components/CardPerson.module.css';
+
+export default function CardPerson({position,photo,name,level, challenges,xp}){
+
+    return(
+        <div className={styles.container}>
+            <div className={styles.position}>
+                <strong> {position} </strong>
+            </div>
+            <div className={styles.data}>
+                <div className={styles.tag}>
+                    <img src={photo} alt="Lucas"/>
+                    <div className={styles.nameLevel}>
+                        <strong>{name}</strong>
+                        <p>
+                            <img src="icons/level.svg" alt="level"/>
+                            Level {level}
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.numbers}>
+                    <div className={styles.challenges}>
+                        <p><strong>{challenges}</strong> completados</p>
+                    </div>
+                    <div className={styles.xp}>
+                        <p><strong>{xp}</strong> xp</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
